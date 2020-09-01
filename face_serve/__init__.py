5e9ddfb36b907b47f7876a5decf7e3d87e2f3030
GinRyan <lp8800lz@gmail.com>
2020-09-01 14:10:03 +0800

    拆分成两个接口中

--- face_serve/__init__.py
+++ face_serve/__init__.py
@@ -6,18 +6,34 @@ import redis
 Minio 连接
 '''
 minioClient = Minio('minio1:9000',
-                  access_key='minioStorage',
-                  secret_key='minioStorage123',
-                  secure=False)
+                    access_key='minioStorage',
+                    secret_key='minioStorage123',
+                    secure=False)
 '''
 bucket name 
 存储桶名称
 '''
 user_face_image_bucket = 'userfaceimage'
 
+
+'''
+姓名列表如果已修改需要重新获取
+'''
+name_list_dirty = 0
+
+
 '''
 Redis 客户端连接
 '''
-redisConn = redis.StrictRedis(host='redis', port=6379, decode_responses=True)   
+redisConn = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
 pipeline = redisConn.pipeline(transaction=True)
 
+
+def reload_all_users():
+    return redisConn.keys('face:*')
+
+
+'''
+所有用户key保存于内存中留待复用
+'''
+all_users = reload_all_users()
--- face_serve/face_recog_service.py
+++ face_serve/face_recog_service.py
@@ -11,9 +11,12 @@ from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
 
 # 分布式存储minio客户端读取器
 from . import minioClient
+from . import reload_all_users
 from . import redisConn
-from . import pipeline
 from . import user_face_image_bucket
+from . import name_list_dirty
+
+
 # You can change this to any folder on your system
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 USE_MINIO = os.environ['USE_MINIO']
@@ -49,6 +52,64 @@ def hello_there(name):
     return content
 
 
+def save_file_and_encode(name, filename, face_image_file, mimetype):
+    '''
+    根据给定的图片文件检测脸并且存在命名为name的目录中。
+
+    :param name: 上传图片用户的名称
+    :param face_image_file: 图片文件路径
+    :param mimetype: 图片文件mimetype
+    :return: 返回可能的脸的用户名称
+    '''
+    ret = {'msg': '', 'code': 0}
+    imagedata = face.load_image_file(face_image_file)
+    '''
+    图片里多少张脸
+    '''
+    faces_count = len(face.face_locations(imagedata))
+    if faces_count == 0:
+        ret['msg'] = 'No face detected!'
+        ret['code'] = -1
+        return jsonify(ret)
+
+    elif faces_count > 1:
+        ret['msg'] = 'Multi faces detected!'
+        ret['code'] = -2
+        return jsonify(ret)
+
+    elif faces_count == 1:
+
+        ret['msg'] = 'Success!'
+        ret['code'] = 0
+        ret['infer'] = 'Unknown face.'
+        # 1、存储这张脸的图片(opt: 可切换开关)
+        if USE_MINIO == "1":
+            # 调用make_bucket来创建一个存储桶。
+            bucket_exist = minioClient.bucket_exists(user_face_image_bucket)
+            if not bucket_exist:
+                minioClient.make_bucket(user_face_image_bucket)
+            try:
+                minioClient.fput_object(bucket_name=user_face_image_bucket, object_name=name + "/" + filename, file_path=face_image_file, content_type=mimetype)
+                ret['code'] = 0
+                ret['msg'] = "File saved!"
+            except ResponseError as err:
+                print(err)
+                ret['code'] = -3
+                ret['msg'] = err.message
+                return jsonify(ret)
+            print('MinIO put file ok.:.:' + face_image_file)
+
+        # 2、存储这张脸的图片的128维脸部描述符编码
+        face_key_name = "face:"+name
+        uploaded_face_encoding_description = face.face_encodings(imagedata)[0]
+        uplist = uploaded_face_encoding_description.tolist()
+        #
+        userface_encoded = json.dumps(uplist)
+        redisConn.lpush(face_key_name, userface_encoded)
+        reload_all_users()
+        return jsonify(ret)
+
+
 @app.route("/upload/<name>", methods=['GET', 'POST'])
 def upload_image_file(name):
     ret = {'msg': '', 'code': 0}
@@ -76,7 +137,7 @@ def upload_image_file(name):
 
             print('Save temp file: ' + tmpfile)
             uploaded_file.save(tmpfile)
-            ret = defect_face_and_save_file(name, filename, tmpfile, uploaded_file.mimetype)
+            ret = save_file_and_encode(name, filename, tmpfile, uploaded_file.mimetype)
             return ret
     elif request.method == 'GET':
         ret['code'] = -10
@@ -84,15 +145,42 @@ def upload_image_file(name):
         return jsonify(ret)
 
 
-def defect_face_and_save_file(name, filename, face_image_file, mimetype):
-    '''
-    根据给定的图片文件检测脸并且存在命名为name的目录中。
+@app.route("/determine/<name>", methods=['POST'])
+def determine_who(name):
+    ret = {'msg': '', 'code': 0}
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            ret['code'] = -1
+            ret['msg'] = "Lack of 'file' ;_; "
+            return jsonify(ret)
 
-    :param name: 上传图片用户的名称
-    :param face_image_file: 图片文件路径
-    :param mimetype: 图片文件mimetype
-    :return: 返回可能的脸的用户名称
-    '''
+        uploaded_file = request.files['file']
+
+        filename = uploaded_file.filename
+
+        if filename == '':
+            filename = name + '.unknown'
+            ret['code'] = -1
+            ret['msg'] = 'File ' + filename + ' not a allowed file.'
+            return jsonify(ret)
+
+        if uploaded_file and allowed_file(filename):
+            filename = str(datetime.now()) + "_" + secure_filename(filename)
+            if not os.path.exists(UPLOAD_FILE_DIR):
+                os.makedirs(UPLOAD_FILE_DIR)
+            tmpfile = UPLOAD_FILE_DIR + filename
+
+            print('Save temp file: ' + tmpfile)
+            uploaded_file.save(tmpfile)
+            ret = encode_and_determine(name, filename, tmpfile, uploaded_file.mimetype)
+            return ret
+    elif request.method == 'GET':
+        ret['code'] = -10
+        ret['msg'] = "You have to use POST method."
+        return jsonify(ret)
+
+
+def encode_and_determine(name, filename, face_image_file, mimetype):
     ret = {'msg': '', 'code': 0}
     imagedata = face.load_image_file(face_image_file)
     '''
@@ -113,52 +201,80 @@ def defect_face_and_save_file(name, filename, face_image_file, mimetype):
         ret['msg'] = 'Success!'
         ret['code'] = 0
         ret['infer'] = 'Unknown face.'
-        # 1、存储这张脸的图片(opt: 可切换开关)
-        if USE_MINIO == "1":
-            # 调用make_bucket来创建一个存储桶。
-            bucket_exist = minioClient.bucket_exists(user_face_image_bucket)
-            if not bucket_exist:
-                minioClient.make_bucket(user_face_image_bucket)
-            try:
-                minioClient.fput_object(bucket_name=user_face_image_bucket, object_name=name + "/" + filename, file_path=face_image_file, content_type=mimetype)
-                ret['code'] = 0
-                ret['msg'] = "File saved!"
-            except ResponseError as err:
-                print(err)
-                ret['code'] = -3
-                ret['msg'] = err.message
-                return jsonify(ret)
-            print('MinIO put file ok.:.:' + face_image_file)
-
-        # 2、存储这张脸的图片的128维脸部描述符编码
+    '''
+    1、如果name不为空，首先取出文件并且获取特征码
+    '''
+    if name != None and name != '':
         face_key_name = "face:"+name
+        '''
+        编码上传的图片待比对
+        '''
         uploaded_face_encoding_description = face.face_encodings(imagedata)[0]
-        uplist = uploaded_face_encoding_description.tolist()
 
-        # 第一步，先从同名用户中取出特征码进行比对
+        '''
+        读出库中的同名面部所有编码
+        '''
         step1Count = redisConn.llen(face_key_name)
         faces_encode_codes = redisConn.lrange(face_key_name, 0, step1Count)
-
         face_code_in_db = []
 
         for face_code in faces_encode_codes:
-            face_code_list = json.loads(face_code)
-            face_code_np = np.array(face_code_list)
+            face_code_vector = json.loads(face_code)
+            face_code_np = np.array(face_code_vector)
             face_code_in_db.append(face_code_np)
-
+        '''
+        开始比对是否和同名编码相同
+        '''
         detected_faces = face.compare_faces(face_code_in_db, uploaded_face_encoding_description)
         if len(detected_faces) > 0:
-            ret['code'] = 1
-            ret['msg'] = 'Found!'
-            ret['name'] = name
-            return jsonify(ret)
+            for detect_face_code in detected_faces:
+                if detect_face_code:
+                    ret['code'] = 1
+                    ret['msg'] = 'Found In Given Name!'
+                    ret['infer'] = name
+                    return jsonify(ret)
 
-        # 第二步，如果第一步找不到，再从其他用户面部特征码中寻找和比对
 
+    # 2、然后取出数据库中的特征码进行比对
+        # 第一步，先从同名用户中取出特征码进行比对
+        # step1Count = redisConn.llen(face_key_name)
+        # faces_encode_codes = redisConn.lrange(face_key_name, 0, step1Count)
 
+        # face_code_in_db = []
 
-        # 第三步，如果都找不到，就存入用户脸编码
-        userface_encoded = json.dumps(uplist)
-        redisConn.lpush(face_key_name, userface_encoded)
+        # for face_code in faces_encode_codes:
+        #     face_code_vector = json.loads(face_code)
+        #     face_code_np = np.array(face_code_vector)
+        #     face_code_in_db.append(face_code_np)
 
-        return jsonify(ret)
+        # detected_faces = face.compare_faces(face_code_in_db, uploaded_face_encoding_description)
+        # if len(detected_faces) > 0:
+        #     ret['code'] = 1
+        #     ret['msg'] = 'Found In Given Name!'
+        #     ret['infer'] = name
+        #     return jsonify(ret)
+
+        # # 第二步，如果第一步找不到，再从其他用户面部特征码中寻找和比对
+        # elif len(detected_faces) == 0:
+        #     face_code_in_db = []
+        #     name_list = redisConn.keys('face:*')
+        #     for name_in_db in name_list:
+        #         name_encoded_face = redisConn.lindex(name_in_db, 0)
+
+        #         face_code_vector = json.loads(name_encoded_face)
+        #         face_code_np = np.array(face_code_vector)
+        #         face_code_in_db.append(face_code_np)
+
+        #     detected_faces = face.compare_faces(face_code_in_db, uploaded_face_encoding_description)
+
+        #     for i, detected_face in enumerate(detected_faces):
+        #         if detected_face:
+        #             ret['code'] = 1
+        #             ret['msg'] = 'Found In All Names!'
+        #             ret['oneshot'] = detected_face
+        #             ret['infer'] = name_list[i]
+        #             return jsonify(ret)
+
+        #     ret['code'] = 0
+        #     ret['msg'] = 'Not Found In All Names!'
+        #     ret['infer'] = len(name_list)
